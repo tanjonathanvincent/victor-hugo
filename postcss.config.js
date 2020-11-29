515df2e649dc51e0699fa09ab690d00e13d2027d
Jonathan Vincent Tan <31546165+tanjonathanvincent@users.noreply.github.com>
2020-11-30 00:28:50 +0800

    Updated PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,16 @@
+const cssnano = require("cssnano");
+
+const prod = process.env.NODE_ENV == "production";
+
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
-      browsers: "last 2 versions"
-    },
-    autoprefixer: {}
-  }
+  plugins: [
+    "postcss-import",
+    "postcss-preset-env",
+    "autoprefixer",
+    prod
+      ? cssnano({
+          preset: "default",
+        })
+      : false,
+  ],
 };
